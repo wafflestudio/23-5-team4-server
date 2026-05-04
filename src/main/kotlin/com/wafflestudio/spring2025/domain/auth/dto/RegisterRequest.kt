8a06dfba03bc11a6aaaf1a6d14cc2b2fa52bedec
Jisu Hong <jaylovegood@snu.ac.kr>
2026-05-04 21:25:14 +0900

    :fire: 안 쓰이는 RegisterRequest 삭제

--- src/main/kotlin/com/wafflestudio/spring2025/domain/auth/dto/RegisterRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.wafflestudio.spring2025.domain.auth.dto
-
-data class RegisterRequest(
-    val email: String,
-    val name: String,
-    val password: String,
-    val profileImage: String? = null,
-)
